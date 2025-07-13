3038d2a30de96003222020875fb7752336805d4c
julietadecia <julietadecia12@gmail.com>
2025-07-13 18:38:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function toggleTachado(elemento) {
+function toggleMateria(elemento) {
+  if (elemento.classList.contains("bloqueada")) return;
+
   elemento.classList.toggle("tachado");
+
+  const materias = document.querySelectorAll(".bloqueada");
+  materias.forEach(materia => {
+    const reqs = materia.dataset.prerrequisitos?.split(",") || [];
+    const cumplido = reqs.every(id => document.getElementById(id).classList.contains("tachado"));
+    if (cumplido) materia.classList.remove("bloqueada");
+  });
 }
